472aad23ef8a68dee31b19bd62e744ae454e6122
tanaka-takayoshi <tanaka-takayoshi@users.noreply.github.com>
2017-07-13 16:17:53 +0900

    Added CronJobExample.

---
+++ CronJobExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace CronJobExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lipsums = File.ReadAllLines("./lipsum.txt");
+            var random = new Random().Next(0, lipsums.Length - 1);
+            Console.WriteLine($"lipusm text: {lipsums[random]}");
+        }
+    }
+}
